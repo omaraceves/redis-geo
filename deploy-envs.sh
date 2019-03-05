ef25654fe40689f0743489a5597d25b7229da45b
omaraceves <lcc.omar.aceves@gmail.com>
2019-03-04 23:06:00 -0800

    Update deploy-envs.sh
    Fixing region

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-west-2a
+export AWS_DEFAULT_REGION=us-west-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=52.38.94.247
 
